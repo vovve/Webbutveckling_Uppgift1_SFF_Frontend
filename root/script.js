abf76013c6e7cd2f46e1c1eb337eb35709f07ebe
vovve <vovve73@icloud.com>
2020-05-14 17:16:54 +0200

    Added Log out function

--- root/script.js
+++ root/script.js
@@ -1,23 +1,68 @@
+console.log(localStorage.getItem("userId"));
 
-var user = "Mia";
+var user = "mia";
 var password = "1234";
 
-var loginButton = document.getElementById("spara");
+var page = document.getElementById("content");
 
-loginButton.addEventListener("click", function()
+if (localStorage.getItem("userId") !== "null")
 {
-    console.log("Knapp")
-    
-    var getUser = document.getElementById("user");
-    var getPass = document.getElementById("password");
-    
-    if (getPass == user && getPass == password)
-    {    
-        console.log("Ja det stämmer")
-    }
-    else
-    {
-        console.log("Nej, något var fel")        
-    }
+    showWelcomePage();
+}
+else
+{
+    showLogInPage
+}
 
-});
+function showWelcomePage()
+{
+    page.innerHTML = "Välkommen!";
+    page.insertAdjacentHTML("beforeend", "<div><button id ='loggoutButton'>Logga ut</button></div>")
+
+    var loggoutButton = document.getElementById("loggoutButton");
+    
+    loggoutButton.addEventListener("click", function()
+    {
+        localStorage.removeItem("userId");
+        showLogInPage();
+    });
+}
+
+function showErrorPage()
+{
+    page.insertAdjacentHTML("afterbegin", "<div>Har du glömt ditt lösenord?</div>");
+}
+
+function showLogInPage()
+{
+    page.innerHTML = "";
+    page.insertAdjacentHTML("afterbegin", 'Namn: <input type="text" id="user"> Lösenord: <input type="password" id="password"> <button id="spara">Logga in</button>');
+
+    var loginButton = document.getElementById("spara");
+
+    loginButton.addEventListener("click", function()
+    {
+        console.log("Knapp")
+        
+        var getUser = document.getElementById("user").value;
+        var getPass = document.getElementById("password").value;
+        
+        if (getUser == user && getPass == password)
+        {    
+            console.log("Ja det stämmer")
+            
+            localStorage.setItem("userId", getUser);
+            console.log(localStorage.getItem("userId"));
+            
+            showWelcomePage();
+        }
+        else
+        {
+            console.log("Nej, något var fel")
+            showErrorPage();
+            
+        }
+    
+    });
+}
+
