b2d9995489387909727f2ccf45a5a6679d092996
vovve <vovve73@icloud.com>
2020-05-14 18:11:47 +0200

    Added fetch

--- root/script.js
+++ root/script.js
@@ -1,8 +1,5 @@
 console.log(localStorage.getItem("userId"));
 
-var user = "mia";
-var password = "1234";
-
 var page = document.getElementById("content");
 
 if (localStorage.getItem("userId") !== "null")
@@ -16,7 +13,21 @@ else
 
 function showWelcomePage()
 {
-    page.innerHTML = "Välkommen!";
+    page.innerHTML = "";
+    var print = "Hej och välkommen "; 
+
+    fetch("users.json")
+    .then(function(response)
+    {
+        return response.json();
+    })
+    .then(function(json)
+    {
+        print = print + json[localStorage.getItem("userId")].userName;
+        page.insertAdjacentHTML("afterbegin", print);
+
+    });
+
     page.insertAdjacentHTML("beforeend", "<div><button id ='loggoutButton'>Logga ut</button></div>")
 
     var loggoutButton = document.getElementById("loggoutButton");
@@ -36,33 +47,41 @@ function showErrorPage()
 function showLogInPage()
 {
     page.innerHTML = "";
-    page.insertAdjacentHTML("afterbegin", 'Namn: <input type="text" id="user"> Lösenord: <input type="password" id="password"> <button id="spara">Logga in</button>');
+    page.insertAdjacentHTML("afterbegin", 'Användarnamn: <input type="text" id="user"> Lösenord: <input type="password" id="password"> <button id="spara">Logga in</button>');
 
     var loginButton = document.getElementById("spara");
 
     loginButton.addEventListener("click", function()
     {
-        console.log("Knapp")
-        
         var getUser = document.getElementById("user").value;
         var getPass = document.getElementById("password").value;
-        
-        if (getUser == user && getPass == password)
-        {    
-            console.log("Ja det stämmer")
-            
-            localStorage.setItem("userId", getUser);
-            console.log(localStorage.getItem("userId"));
-            
-            showWelcomePage();
-        }
-        else
-        {
-            console.log("Nej, något var fel")
-            showErrorPage();
-            
-        }
-    
+
+        fetch("users.json")
+            .then(function(response)
+            {
+                return response.json();
+            })
+            .then(function(json)
+            {
+                console.log(json);
+
+                for(i=0; i<json.length; i++)
+                {
+                if (getUser == json[i].userLogin && getPass == json[i].userPassword)
+                {    
+                    console.log("Ja det stämmer")                   
+                    localStorage.setItem("userId", i);
+                }             
+            }
+            if(localStorage.getItem("userId") !== null) 
+            {
+                showWelcomePage();
+            }
+            else
+            {
+                showErrorPage();
+            }
+        });    
     });
 }
 
