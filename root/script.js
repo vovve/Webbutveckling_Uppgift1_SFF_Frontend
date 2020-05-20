b50a089adf54949cff8354e069c60f5547a73070
vovve <vovve73@icloud.com>
2020-05-20 14:53:51 +0200

    Removed html, added js. Not working properly

--- root/script.js
+++ root/script.js
@@ -1,165 +1,186 @@
+var page = document.getElementById("content");
+var login = document.getElementById("login");
+
+login.addEventListener("click", function(){
+    console.log("Klick på knappen");
+    showLogInPage();
+});
 // Log in functions
-console.log(localStorage.getItem("userId"));
-
-var login = document.getElementById("content");
-
-if (localStorage.getItem("userId") !== "null")
-{
-    showWelcomePage();
+function showLogInPage(){
+    page.innerHTML = "";
+    page.insertAdjacentHTML("afterbegin", 
+    '<center>Användarnamn: <input type="text" id="user"> Lösenord: <input type="password" id="password"> <button id="spara">Logga in</button></center>');
+    
+    var loginButton = document.getElementById("spara");
+    
+    loginButton.addEventListener("click", function(){
+        var getUser = document.getElementById("user").value;
+        var getPass = document.getElementById("password").value;
+        
+        fetch("https://localhost:5001/api/filmstudio")
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json){
+            for(i=0; i<json.length; i++){
+                if (getUser == json[i].Name && getPass == json[i].Password){    
+                    console.log("Ja det stämmer")                   
+                    localStorage.setItem("userId", i);
+                }             
+                if(localStorage.getItem("userId") !== null){
+                showWelcomePage();
+                }
+                else{
+                showErrorPage();
+                }
+            }
+        });    
+    });
 }
-else
-{
-    showLogInPage
+function showErrorPage(){
+    page.insertAdjacentHTML("afterbegin", "<div>Har du glömt ditt lösenord?</div>");
 }
-
-function showWelcomePage()
-{
-    login.innerHTML = "";
+function showWelcomePage(){
     var print = "Välkommen "; 
-
+    
     fetch("users.json")
-    .then(function(response)
-    {
+    .then(function(response){
         return response.json();
     })
-    .then(function(json)
-    {
+    .then(function(json){
+        page.innerHTML = "";
         print = print + json[localStorage.getItem("userId")].userName + "!";
-        login.insertAdjacentHTML("afterbegin", print);
-
+        page.insertAdjacentHTML("afterbegin", "<center><div>" + print + "</div></center>");
+        page.insertAdjacentHTML("beforeend", "<center><div><button id ='loggoutButton'>Logga ut</button></div></center>");
     });
-
-    login.insertAdjacentHTML("beforeend", "<div><button id ='loggoutButton'>Logga ut</button></div>")
-
+    
     var loggoutButton = document.getElementById("loggoutButton");
     
-    loggoutButton.addEventListener("click", function()
-    {
+    loggoutButton.addEventListener("click", function(){
         localStorage.removeItem("userId");
         showLogInPage();
     });
 }
 
-function showErrorPage()
-{
-    login.insertAdjacentHTML("afterbegin", "<div>Har du glömt ditt lösenord?</div>");
+var newlogin = document.getElementById("newlogin");
+
+newlogin.addEventListener("click", function(){
+    addItem();
+});
+
+function addItem(Name, Password, Verified){
+    console.log("Lägg till");
+    newlogin.insertAdjacentHTML("afterbegin", 
+    '<center>Användarnamn: <input type="text" id="user"> Lösenord: <input type="password" id="password"> <button id="spara">Spara</button></center>');
+    
+    var data = { Name: Name, Password: Password, Verified: Verified};
+    newlogin = document.getElementById("newlogin").value;
+     
+    fetch('https://localhost:5001/api/filmstudio',{
+        method: 'POST',
+        headers:{
+            'content-type': 'application/json',
+           },
+        body: JSON.stringify(data),
+    })
+        .then(response => response.json())
+        .then(data  =>{
+           console.log('success:' + data);
+        })
+        .catch((error) =>{
+           console.error('error', error);
+        });  
 }
 
-function showLogInPage()
-{
-    login.innerHTML = "";
-    login.insertAdjacentHTML("afterbegin", 'Användarnamn: <input type="text" id="user"> Lösenord: <input type="password" id="password"> <button id="spara">Logga in</button>');
+//-------------------------------------------------------------------------------
+// Method to print a list of films and trivia
 
-    var loginButton = document.getElementById("spara");
+var movies = document.getElementById("movies");
 
-    loginButton.addEventListener("click", function()
-    {
-        var getUser = document.getElementById("user").value;
-        var getPass = document.getElementById("password").value;
-
-        fetch("users.json")
-            .then(function(response)
-            {
-                return response.json();
-            })
-            .then(function(json)
-            {
-                console.log(json);
-
-                for(i=0; i<json.length; i++)
-                {
-                if (getUser == json[i].userLogin && getPass == json[i].userPassword)
-                {    
-                    console.log("Ja det stämmer")                   
-                    localStorage.setItem("userId", i);
-                }             
-            }
-            if(localStorage.getItem("userId") !== null) 
-            {
-                showWelcomePage();
-            }
-            else
-            {
-                showErrorPage();
-            }
-        });    
-    });
-}
-//__________________________________________________________________
-// Method to print a list of films
+movies.addEventListener("click", function(){
+    console.log("Klick på filmknappen");
+    printfilmList();
+    printtriviaList();
+});
 
 var filmList = document.getElementById("filmlist");
 
-printfilmList();
+function printfilmList(){
+    fetch("https://localhost:5001/api/film")
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(json){
+        console.log("printfilmList", json);
+        
+        for (i = 0; i < json.length; i++){
+            console.log(json[i].name);
+            filmlist.insertAdjacentHTML("beforeend", '<div class = "movieposter">' + json[i].name + '</div>', 
+            '<img src="wwwroot/img/bio.jpg "alt="placeholder för omslagsbild" width="25%" height="25%">');
+        }
+    });
+}
 
-function printfilmList()
-{
-    fetch("https://localhost:5001/api/Film")
+var triviaList = document.getElementById("trivialist");
+
+function printtriviaList(movieId){
+    console.log(movieId);
+
+    fetch("https://localhost:5001/api/filmTrivia")
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(json){
+        console.log("printtriviaList", json);
+        
+        for (i=0; i < json.length; i++){
+            if(movieId == json[i].FilmId){
+                console.log(json[i].trivia);
+                trivialist.insertAdjacentHTML("beforeend", '<div class = "movieposter">' + json[i].trivia + '</div>');
+            }            
+        }
+    });
+}
+// Method to rent and return (delete) a movie
+function rentedFilm(){
+    fetch("https://localhost:5001/api/rentedFilm")
     .then(function(response)
     {
         return response.json();
     })
     .then(function(json)
     {
-        console.log("printfilmList", json);
-        
-        filmList.innerHTML = "";
+        console.log("rentedFilm", json);
         
         for (i=0; i < json.length; i++)
         {
-            console.log(json[i].name);
-            filmList.insertAdjacentHTML("beforeend", "<div><button onclick = 'deleteItem(" + json[i].id + ")'>" + json[i].name + "</button></div>")
+            if(movieId == json[i].FilmId){
+                console.log(json[i].Id);
+                rentedFilm.insertAdjacentHTML("beforeend", "<div>onclick = 'deleteItem(" + json[i].id + ")'>" + json[i].id + '</div>');
+            }            
         }
     });
-};
+}
 
+function returnMovie(id){
+    console.log("Returnera", id);
 
-// Method to add and save a new filmstudio
-var saveButton = document.getElementById("savefilmStudio");
-saveButton.addEventListener("click", function()
-{
-   Filmstudio = document.getElementById("Filmstudio").value;
-   addItem(Filmstudio, fyra);
-});
- 
-function addItem(Name, Password, Verified)
-{
-   console.log("Lägg till")
- 
-   var data = { Name: Name, Password: Password, Verified: Verified};
- 
-   fetch('https://localhost:5001/api/filmstudio', 
-   {
-       method: 'POST',
-       headers:
-       {
-           'content-type': 'application/json',
-       },
-       body: JSON.stringify(data),
-   })
-   .then(response => response.json())
-   .then(data  =>
-   {
-       console.log('sucsess:' + data);
-   })
-   .catch((error) =>
-   {
-       console.error('error', error);
-   });
-  
-};
-
-
-// Method to delete a movie
-
-function deleteItem(id)
-{
-    console.log("Radera", id)
-
-    fetch('https://localhost:5001/api/Film/' + id, 
-    {
+    fetch('https://localhost:5001/api/rentedFilm/' + id,{
         method: 'DELETE', 
     })
     .then(response => response.json())
-    .then(response => printfilmList())
+    .then(response => rentedFilm());
+}
+
+//-------------------------------------------------------------------------------
+var page = document.getElementById("content");
+var contact = document.getElementById("contact");
+
+contact.addEventListener("click", function(){
+    console.log("Klick på kontaktknappen");
+    printContactInfo();
+});
+
+function printContactInfo(){
+    page.insertAdjacentHTML("afterbegin", '<center><div class = "contact">Kontakta oss</div></center>');
 }
